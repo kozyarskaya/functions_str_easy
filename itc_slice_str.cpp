7e383afbfa0eecfd86f1b370b2fdaf32aef2a476
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2021-10-15 09:23:52 +0300

    Update itc_slice_str.cpp

--- itc_slice_str.cpp
+++ itc_slice_str.cpp
@@ -4,12 +4,15 @@ string itc_slice_str(string str, int start, int finish){
     long long len = itc_len(str);
     string new_str = "";
     if (finish >= len){
-        for (long long i = start - 1; i < len; i++){
+        for (long long i = start; i < len; i++){
             new_str += str[i];
         }
     }
+    if (start >= len){
+        return str;
+    }
     else{
-        for (long long i = start - 1; i < finish; i++){
+        for (long long i = start; i <= finish; i++){
             new_str += str[i];
         }
     }
