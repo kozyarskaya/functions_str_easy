dca01e91f12d37ede9c32d97d54ea53119f43371
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2021-10-15 10:24:01 +0300

    Update itc_first_end_three.cpp

--- itc_first_end_three.cpp
+++ itc_first_end_three.cpp
@@ -3,13 +3,13 @@
 void itc_first_end_three(string str){
     long long len = itc_len(str);
     if (len > 5){
-        cout << str[0] << str[1] << str[2] << endl;
-        cout << str[len - 3] << str[len - 2] << str[len - 1] << endl;
+        cout << str[0] << str[1] << str[2];
+        cout << str[len - 3] << str[len - 2] << str[len - 1];
 
     }
     else{
         for (long long i = 0; i < len; i++){
-            cout << str[0] << endl;
+            cout << str[0];
         }
     }
 }
