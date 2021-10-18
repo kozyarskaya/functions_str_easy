b2e0f28846050be0062184d5b209287d4a69b2d7
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2021-10-18 12:47:10 +0300

    Update itc_even_place.cpp

--- itc_even_place.cpp
+++ itc_even_place.cpp
@@ -3,10 +3,11 @@
 string itc_even_place(string str){
     long long i = 1;
     string new_str = "";
+   if (itc_len(str) < 2)
+        return "-1";
     while(str[i] != '\0'){
         new_str += str[i];
         i += 2;
     }
     return new_str;
- return -1;
 }
