5460dd3eed5bcdb6df874553e4d58d265bc0c007
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2021-11-06 21:34:18 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -2,14 +2,24 @@
 
 string itc_cmp_str(string str1, string str2, int num){
     string new_str = "";
-    long long len = itc_len(str1);
-    for (int i = 0; i < num - 1; i++){
-        new_str += str1[i];
+    long long len1 = itc_len(str1);
+    long long len2 = itc_len(str2);
+    if (len2 > ((len1) - num)){
+        new_str += itc_slice_str(str1, 0, num - 2);
+        new_str += itc_slice_str(str2, 0, (len1 - num));
     }
+        
+    else{
+        for (int i = 0; i < num - 1; i++){
+            new_str += str1[i];
+        }
 
-    new_str += str2;
-    for (int j = num - 1 ; j < len; j++){
-        new_str += str1[j];
-    }
-    return itc_slice_str(new_str, 0, len - 1);
+        new_str += str2;
+        for (int j = num - 1 ; j < len1; j++){
+            new_str += str1[j];
+            }
+        
+        }
+    return itc_slice_str(new_str, 0, len1 - 1);
+return "-1";
 }
